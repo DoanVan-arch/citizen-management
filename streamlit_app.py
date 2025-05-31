632d248bc841e481f1cc48f788747216f5127359
DoanVan-arch <skypro311294@gmail.com>
2025-05-31 21:31:34 +0700

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -490,7 +490,7 @@ def scan_qr_code():
         "height": {"min": 720, "ideal": 1080, "max": 2160},
         "frameRate": {"min": 15, "ideal": 30, "max": 60}
     },
-    "audio": false
+    "audio": False
 },
 
                     async_processing=False,
